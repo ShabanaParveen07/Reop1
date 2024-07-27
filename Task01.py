235f5e20772761eb2cd73076d8120d2a4364ebac
ShabanaParveen07 <skparveen.it@gmail.com>
2024-07-27 20:50:24 +0530

    saving

---
+++ Task01.py
@@ -0,0 +1,6 @@
+name = input("Enter your name")
+age= input("Enter your age")
+Roll_no= input("Enter your Roll number")
+phone_no= input("Enter your phone numer")
+
+print(name, age, Roll_no, phone_no)
